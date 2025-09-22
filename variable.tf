e2bbeb9d356572c269e8fc53dc18f7bf9f1d84ff
pratish-21 <jangampratish1@gmail.com>
2025-09-22 12:38:02 +0530

    terraform

--- variable.tf
+++ variable.tf
@@ -3,3 +3,19 @@ variable "environment"{
     description = "env type"
     default = "Staging"
 }
+variable "storage-disk"{
+    type = number
+    description = "disk size"
+    default = 32
+}
+variable "delete-disk"{
+    type = bool
+    description = "deletion of disk"
+    default = true
+}
+
+variable "allowed-locations"{
+    type = list(string)
+    description = "locations"
+    default = [ "West Europe" , "North Europe" , "East US" ]
+}
